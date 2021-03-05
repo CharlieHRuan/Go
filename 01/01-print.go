f95a939571ba8e6dd80a3f99281c426889eed625
RuanHeng <heng_ruan@subin.cn>
2021-03-05 14:22:11 +0800

    整理代码

--- 01/01-print.go
+++ 01/01-print.go
@@ -1,14 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	//常量
-	const lightSpeed = 111222
-	// 变量
-	var distance = 100000099
-	fmt.Println(distance/lightSpeed, "seconds")
+	fmt.Println("Hello, playground")
+	fmt.Println("Hello")
 
-	distance = 401000000
-	fmt.Println(distance/lightSpeed, "seconds")
+	fmt.Print("Hello,23")
+	fmt.Println("Hello,34")
+
+	fmt.Printf("体重是 %v 磅", 149.0*0.3783)
+	fmt.Printf("体重是 %v 磅\n", 149.0*0.3783)
+
+	fmt.Printf("体重在%v是 %v 磅\n", "火星", 149.0*0.3783)
+
+	//对齐文本
+	//-向右填充，+向左填充
+	fmt.Printf("%-15v $%4v\n", "SpaceX", 94)
+	fmt.Printf("%-15v $%4v\n", "Virgin Galactic", 100)
+	/*
+		SpaceX          $  94
+		Virgin Galactic $ 100
+	*/
 }
